56fbb632439b3e00bf72f806cd12035dbdb346c5
Antoine M <mojdril-apps@yahoo.fr>
2011-08-28 17:51:29 +0200

    args kata

---
+++ args/args.md
@@ -0,0 +1,10 @@
+Args
+===
+
+This is a kata that you can find [here](http://codingdojo.org/cgi-bin/wiki.pl?KataArgs). 
+
+## Lessons learned
+* Hashmaps in racket are nice but they are not a list of pairs, so no car or cdr.
+* I came to make a small proto-object to handle specs, but OO means imperative. It's often fast and convenient, but not far from quick and dirty.
+* At first the simplest solution that came out was the very imperative parse* function. During a refactoring in the end I came to a more functional thing by creating a utility function used to map a function over a hashmap. I kept parse* as a reminder.
+* I could have spent time clearly defining the entities used in the program : flags, argument values, default values, etc, but the timebox was over. Maybe next time.
---
+++ args/args.rkt
@@ -0,0 +1,87 @@
+#lang racket
+
+(require rackunit)
+
+;(define argstring "-l -p 8080 -d /usr/logs")
+(define flag-id-regexp #px"-[\\D]")
+(define (flag? string)
+  (regexp-match? flag-id-regexp string))
+
+(define (undefined? x) (null? x))
+
+(define (integer-parser arg)
+  (if (undefined? arg) 0 (string->number arg))) 
+(define (string-parser arg)
+  (if (undefined? arg) "default string" arg))
+(define (boolean-parser arg) #t)
+
+(define (new-spec)
+  (let ((specdata (hash)))
+    (define (this sel . args)
+      (case sel
+        ((add-binding) (set! specdata (hash-set specdata (car args) (cadr args))))
+        ((value) specdata)
+        ((empty) (set! specdata (hash)))))
+    this))
+
+(define (string->args-rec argslist)
+  (cond ((null? argslist) (hash))
+        ((and (flag? (first argslist)) (not (flag? (second argslist))))
+         (hash-set (string->args-rec (cddr argslist)) (first argslist) (second argslist)))
+        (else (hash-set (string->args-rec (cdr argslist)) (first argslist) '()))))
+
+(define (string->args arg-string)
+  (string->args-rec (regexp-split #px" " arg-string)))
+
+(define (hash-map* a-hash func)
+  (define (hash-map-rec* keys res)
+    (cond ((null? keys) res)
+          (else
+           (let* ((key (car keys))
+                  (value (hash-ref a-hash key)))
+             (hash-map-rec* (cdr keys) (hash-set res key (func key value)))))))
+  (hash-map-rec* (hash-keys a-hash) (hash)))
+
+(define (parse arg-string spec)
+  (let ((argmap (string->args arg-string)))
+    (define (func key value)
+      (let ((value (hash-ref argmap key '())))
+        ((hash-ref spec key) value)))
+    (hash-map* spec func)))
+
+(define (parse* arg-string spec)
+  (define argsmap (string->args arg-string))
+  (define result (hash))
+  (let loop ((next-flags (hash-keys spec)))
+    (let ((value (hash-ref argsmap (car next-flags) '()))
+          (parser (hash-ref spec (car next-flags))))
+      (set! result (hash-set result (car next-flags) (parser value)))
+      (if (null? (cdr next-flags)) result
+          (loop (cdr next-flags))))))
+
+(test-begin "test"
+ (define argstring "-l -p 8080 -d /usr/logs")
+ (define spec (new-spec))
+ (spec 'add-binding "-p" integer-parser)
+ (spec 'add-binding "-l" boolean-parser)
+ (spec 'add-binding "-d" string-parser)
+ (check-equal? (hash-keys (spec 'value)) '("-d" "-l" "-p"))
+ (check-equal? (string->args argstring) #hash(("-d" . "/usr/logs") ("-l" . ()) ("-p" . "8080")))
+ (check-equal? (flag? "-d") #t)
+ (check-equal? (hash-map* #hash((10 . 1) (11 . 2)) (lambda (key value) (+ key value))) #hash((10 . 11) (11 . 13)))
+ (check-equal? (parse argstring (spec 'value)) #hash(("-d" . "/usr/logs") ("-l" . #t) ("-p" . 8080)))
+ (check-equal? (parse* argstring (spec 'value)) #hash(("-d" . "/usr/logs") ("-l" . #t) ("-p" . 8080))))
+
+(test-begin "test2"
+ (define argstring "-l -p 8080")
+ (define spec (new-spec))
+ (spec 'add-binding "-p" integer-parser)
+ (spec 'add-binding "-l" boolean-parser)
+ (spec 'add-binding "-d" string-parser)
+ (check-equal? (hash-keys (spec 'value)) '("-d" "-l" "-p"))
+ (check-equal? (string->args argstring) #hash(("-l" . ()) ("-p" . "8080")))
+ (check-equal? (flag? "-d") #t)
+ (check-equal? (hash-map* #hash((10 . 1) (11 . 2)) (lambda (key value) (+ key value))) #hash((10 . 11) (11 . 13)))
+ (check-equal? (parse argstring (spec 'value)) #hash(("-d" . "default string") ("-l" . #t) ("-p" . 8080)))
+ (check-equal? (parse* argstring (spec 'value)) #hash(("-d" . "default string") ("-l" . #t) ("-p" . 8080))))
+

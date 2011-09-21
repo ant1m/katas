5baa3d522ec4d2501e0f5601b636b1e232033764
Antoine M <mojdril-apps@yahoo.fr>
2011-09-21 23:07:38 +0200

    FizzBuzz in fantom

--- codebreaker/CodeBreaker.hs
+++
@@ -1,3 +0,0 @@
-module CodeBreaker where
-
-mark tentative secret = ""
--- codebreaker/CodeBreakerTests.hs
+++
@@ -1,9 +0,0 @@
-module CodeBreakerTests where
-
-import Test.HUnit
-import CodeBreaker
-
-tests = 
-  TestList ["" ~?= (mark "rrrr" "gggg"),
-            True ~?= True] 
-main = do runTestTT tests
